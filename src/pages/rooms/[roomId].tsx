db95731fc5d7c866f8c2b4426f256d0f19413c83
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-07-18 22:34:28 +0200

    styling

--- src/pages/rooms/[roomId].tsx
+++ src/pages/rooms/[roomId].tsx
@@ -12,7 +12,26 @@ function MessageItem({
   message: Message;
   session: Session;
 }) {
-  return <li>{message.message}</li>;
+  const baseStyles =
+    'mb-4 text-md w-7/12 p-4 text-gray-700 border border-gray-700 rounded-md';
+  const liStyles =
+    message.sender.name === session.user?.name
+      ? baseStyles.concat(' self-end bg-gray-700 text-white')
+      : baseStyles;
+
+  return (
+    <li className={liStyles}>
+      <div className='flex flex-col'>
+        <time>
+          {message.sentAt.toLocaleTimeString('en-GB', {
+            timeStyle: 'short',
+          })}{' '}
+          - {message.sender.name}
+        </time>
+        {message.message}
+      </div>
+    </li>
+  );
 }
 
 function RoomPage() {
@@ -42,13 +61,14 @@ function RoomPage() {
     );
 
   return (
-    <div>
-      <ul>
+    <div className='p-2 h-screen flex flex-col gap-4'>
+      <ul className='flex flex-col flex-1 justify-end'>
         {messages.map((m) => (
           <MessageItem key={m.id} message={m} session={session} />
         ))}
       </ul>
       <form
+        className='flex gap-2'
         onSubmit={(e) => {
           e.preventDefault();
           sendMessageMutation({ roomId, message });
@@ -56,11 +76,17 @@ function RoomPage() {
         }}
       >
         <textarea
+          className='p-2 w-full text-gray-700 bg-gray-50 rounded-md border border-gray-700'
           value={message}
           onChange={(e) => setMessage(e.target.value)}
           placeholder='What do you want to say'
         />
-        <button type='submit'>Send message</button>
+        <button
+          className='flex-1 text-white bg-gray-900 px-2 py-1 rounded-md'
+          type='submit'
+        >
+          Send message
+        </button>
       </form>
     </div>
   );
