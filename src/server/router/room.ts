18c984f608fee51500556933e6a20b26dee5cd4d
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-07-18 20:06:54 +0200

    create ws sendmessage

--- src/server/router/room.ts
+++ src/server/router/room.ts
@@ -1,4 +1,46 @@
 import { createRouter } from './context';
-import { z } from 'zod';
+import {
+  sendMessageSchema,
+  messageSubSchema,
+  Message,
+} from '../../constants/schema';
+import { nanoid } from 'nanoid';
+import { Events } from '../../constants/events';
+import * as trpc from '@trpc/server';
 
-export const roomRouter = createRouter();
+export const roomRouter = createRouter()
+  .mutation('send-message', {
+    input: sendMessageSchema,
+    resolve: ({ ctx, input }) => {
+      const message: Message = {
+        id: nanoid(),
+        ...input,
+        sentAt: new Date(),
+        sender: {
+          name: ctx.session?.user?.name || 'unknown',
+        },
+      };
+
+      ctx.ee.emit(Events.SEND_MESSAGE, message);
+
+      return true;
+    },
+  })
+  .subscription('onSendMessage', {
+    input: messageSubSchema,
+    resolve: ({ ctx, input }) => {
+      return new trpc.Subscription<Message>((emit) => {
+        function onMessage(data: Message) {
+          if (input.roomId === data.roomId) {
+            emit.data(data);
+          }
+        }
+
+        ctx.ee.on(Events.SEND_MESSAGE, onMessage);
+
+        return () => {
+          ctx.ee.off(Events.SEND_MESSAGE, onMessage);
+        };
+      });
+    },
+  });
